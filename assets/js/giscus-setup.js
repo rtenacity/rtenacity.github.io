5a43564b039529c6c39a25f8163f085fb3512612
rtenacity <50297222+rtenacity@users.noreply.github.com>
2025-12-20 22:09:21 +0000

    Deploying to gh-pages from @ rtenacity/rtenacity.github.io@698b92e99a2e3df44eac2558446ff7c5186ec9b6 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "rtenacity/test-website",
+    "data-repo": "rtenacity/rtenacity.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
